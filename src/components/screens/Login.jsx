869f78c920f983c47f9f4aed76f0ea52ddf1bcc9
nikhil <nikicvr@gamail.com>
2023-07-11 13:50:11 +0530

    "working on login"

--- src/components/screens/Login.jsx
+++ src/components/screens/Login.jsx
@@ -2,14 +2,38 @@ import React, { useState } from "react";
 import FromRow from "../includes/FromRow";
 
 function Login() {
-    const userDetails = useState({
-        name: "",
-        email: "",
+    const [userDetails, setUserDetails] = useState({
+        userName: "",
         password: "",
     });
+
+    const handleChange = (e) => {
+        setUserDetails({ ...userDetails, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(userDetails);
+    };
+
     return (
         <div>
-            <FromRow name="email" />
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <FromRow
+                    name="userName"
+                    type="text"
+                    handleChange={handleChange}
+                    value={userDetails.userName}
+                />
+                <FromRow
+                    name="password"
+                    type="password"
+                    handleChange={handleChange}
+                    value={userDetails.password}
+                />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 }
--- src/context/Store.js
+++ src/context/Store.js
@@ -1,7 +1,10 @@
 import { createContext, useContext, useReducer } from "react";
 import { reducer } from "./reducer";
+import axios from "axios";
+import { SETUP_USER_BEGIN } from "./actions";
 
 const initialState = {
+    loading: false,
     isLogedIn: false,
 };
 
@@ -10,8 +13,16 @@ const AppContext = createContext(initialState);
 export const Provider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
 
+    const authFetch = axios.create({
+        baseURL: "https://fakestoreapi.com",
+    });
+
+    const LoginUser = async () => {
+        dispatch({ type: SETUP_USER_BEGIN });
+    };
+
     return (
-        <AppContext.Provider value={{ ...state, dispatch }}>
+        <AppContext.Provider value={{ ...state }}>
             {children}
         </AppContext.Provider>
     );
